496e3d66d4c20802e5770616299b65204fcf118c
Pedro Venâncio <ph.venancio2@gmail.com>
2024-04-29 20:53:28 -0300

    view: Adding new page  "Sobre nós"

--- src/app/modules/sobre/index/index.component.ts
+++ src/app/modules/sobre/index/index.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { ThemeModeService } from '../../../utils/services/theme-mode.service';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass,
+    NgIf
+  ],
   templateUrl: './index.component.html',
   styleUrl: './index.component.sass'
 })
 export class IndexComponent {
+  public theme = 'dark';
+
+  constructor(private themeMode : ThemeModeService) {
+    this.themeMode.getThemeMode().subscribe((theme) => {
+      this.theme = theme;
+    });
+  }
 
 }
