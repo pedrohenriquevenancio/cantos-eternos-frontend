611fe7c34b0d52a8bcbf4ae0db35cafbb6f35c1b
Pedro Venâncio <ph.venancio2@gmail.com>
2024-06-02 17:35:02 -0300

    refactor: Applying local storage to theme mode service

--- src/app/utils/services/theme-mode.service.ts
+++ src/app/utils/services/theme-mode.service.ts
@@ -8,13 +8,20 @@ export class ThemeModeService {
   private themeSubject : BehaviorSubject<string> = new BehaviorSubject<string>('dark');
   public theme$ : Observable<string> = this.themeSubject.asObservable();
 
-  constructor() { }
+  constructor() {
+    if (typeof window !== 'undefined' && localStorage.getItem('theme')) {
+      this.themeSubject.next(localStorage.getItem('theme') as string);
+    }
+  }
 
   getThemeMode () {
     return this.theme$;
   }
 
   setThemeMode (theme : string) {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('theme', theme);
+    }
     this.themeSubject.next(theme);
   }
 
